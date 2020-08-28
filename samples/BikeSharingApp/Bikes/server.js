31e050740b71d737667f9591dc29c129ccddb9ff
Anupama Tuli <anutuli@microsoft.com>
2020-08-28 16:03:14 -0700

    new version

--- samples/BikeSharingApp/Bikes/server.js
+++ samples/BikeSharingApp/Bikes/server.js
@@ -230,8 +230,7 @@ app.get('/api/bikes/:bikeId', function(req, res) {
             return;
         }
 
-        var theBike = result;
-                
+        var theBike = result;                
         // Hard code image url *FIX ME*        
         //theBike.imageUrl = "/static/logo.svg";
         theBike.id = theBike._id;
